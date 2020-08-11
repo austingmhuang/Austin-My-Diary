61c36252f041182609b7e1027bbc47dbdbf39f26
Austin Huang <agh3gd@virginia.edu>
2020-08-11 01:01:29 -0400

    Changed a const to a let

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated} = require("../config/auth");
 const Entry = require('../models/entry');
-const userId;
+let userId;
 
 router.get('/entries', ensureAuthenticated, (req, res) => {
     function next() {
